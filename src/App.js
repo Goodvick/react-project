0178e0c0f4a113252d897149af3fccac7956599a
Goodvick <teplicov23@gmail.com>
2026-03-04 22:45:07 +0300

    update 1.2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './style/NullStyle.css';
 import './style/App.css';
 import PostList from "./conponents/PostList";
 import MyButton from "./conponents/UI/button/MyButton";
+import MyInput from "./conponents/UI/input/MyInput";
 
 function App() {
     const [posts1, setPosts1] = useState([
@@ -10,18 +11,34 @@ function App() {
         { id: 2, title: 'JavaScript', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
         { id: 3, title: 'JavaScript', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
     ])
-    const [posts2, setPosts2] = useState([
-        { id: 1, title: 'C++', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
-        { id: 2, title: 'C++', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
-        { id: 3, title: 'C++', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
-    ])
+    // const [posts2, setPosts2] = useState([
+    //     { id: 1, title: 'C++', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
+    //     { id: 2, title: 'C++', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
+    //     { id: 3, title: 'C++', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
+    // ])
+    const [post, setPost] = useState({ title: '', body: '' })
+    const addNewPost = (e) => {
+        e.preventDefault()
+        setPosts1([...posts1, { ...post, id: Date.now() }])
+        setPost({ title: '', body: '' })
+    }
+
+
 
     return (
         <div className="App">
             <form>
-                <input type="text" placeholder="Название поста: " />
-                <input type="text" placeholder="Описание поста: " />
-                <MyButton disabled>Создать пост</MyButton>
+                <MyInput
+                    value={post.title}
+                    onChange={e => setPost({ ...post, title: e.target.value })}
+                    type="text"
+                    placeholder="Название поста" />
+                <MyInput
+                    value={post.body}
+                    onChange={e => setPost({ ...post, body: e.target.value })}
+                    type="text"
+                    placeholder="Описание поста" />
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
             </form>
             <PostList posts={posts1} title='Список постов № 1' />
             {/* <PostList posts={posts2} title='Список постов № 2' /> */}
