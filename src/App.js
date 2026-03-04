a5456acff52222d1ddd38ebb646f92fe140be5d7
Goodvick <teplicov23@gmail.com>
2026-03-04 21:33:35 +0300

    update 1.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
+import './style/NullStyle.css';
+import './style/App.css';
+import PostList from "./conponents/PostList";
+import MyButton from "./conponents/UI/button/MyButton";
 
 function App() {
-    const [likes, setLikes] = useState(0)
-    const [value, setValue] = useState('Text')
-
-    function increment() {
-        setLikes(likes + 1)
-    }
-    function decrement() {
-        setLikes(likes - 1)
-    }
+    const [posts1, setPosts1] = useState([
+        { id: 1, title: 'JavaScript', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
+        { id: 2, title: 'JavaScript', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
+        { id: 3, title: 'JavaScript', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
+    ])
+    const [posts2, setPosts2] = useState([
+        { id: 1, title: 'C++', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
+        { id: 2, title: 'C++', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
+        { id: 3, title: 'C++', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
+    ])
 
     return (
         <div className="App">
-            <h1>{likes}</h1>
-            <h1>{value}</h1>
-            <input type="text" value={value} onChange={event => setValue(event.target.value)} />
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}> Decrement</button>
+            <form>
+                <input type="text" placeholder="Название поста: " />
+                <input type="text" placeholder="Описание поста: " />
+                <MyButton disabled>Создать пост</MyButton>
+            </form>
+            <PostList posts={posts1} title='Список постов № 1' />
+            {/* <PostList posts={posts2} title='Список постов № 2' /> */}
         </div >
     );
 }
