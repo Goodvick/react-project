546b8e141331c2acc8408935d559f2a6472c998e
Goodvick <teplicov23@gmail.com>
2026-03-11 23:18:21 +0300

    update 1.3

--- src/App.js
+++ src/App.js
@@ -2,46 +2,46 @@ import React, { useState } from "react";
 import './style/NullStyle.css';
 import './style/App.css';
 import PostList from "./conponents/PostList";
-import MyButton from "./conponents/UI/button/MyButton";
-import MyInput from "./conponents/UI/input/MyInput";
+import PostForm from "./conponents/PostForm";
+import MySelect from "./conponents/UI/select/MySelect";
 
 function App() {
-    const [posts1, setPosts1] = useState([
-        { id: 1, title: 'JavaScript', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
-        { id: 2, title: 'JavaScript', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
-        { id: 3, title: 'JavaScript', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
+    const [posts, setPosts] = useState([
+        { id: 1, title: 'aa', body: '1Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
+        { id: 2, title: 'vv', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
+        { id: 3, title: 'bb', body: '2Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
     ])
-    // const [posts2, setPosts2] = useState([
-    //     { id: 1, title: 'C++', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
-    //     { id: 2, title: 'C++', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
-    //     { id: 3, title: 'C++', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores non quasi perspiciatis? Ipsum omnis assumenda quos, placeat officia maiores dolore cum tempore. Aperiam nobis architecto consectetur labore, tempora perferendis necessitatibus.' },
-    // ])
-    const [post, setPost] = useState({ title: '', body: '' })
-    const addNewPost = (e) => {
-        e.preventDefault()
-        setPosts1([...posts1, { ...post, id: Date.now() }])
-        setPost({ title: '', body: '' })
+    const [selectedSort, setselectedSort] = useState('')
+    const sortPost = (sort) => {
+        setselectedSort(sort)
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
     }
-
-
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
 
     return (
         <div className="App">
-            <form>
-                <MyInput
-                    value={post.title}
-                    onChange={e => setPost({ ...post, title: e.target.value })}
-                    type="text"
-                    placeholder="Название поста" />
-                <MyInput
-                    value={post.body}
-                    onChange={e => setPost({ ...post, body: e.target.value })}
-                    type="text"
-                    placeholder="Описание поста" />
-                <MyButton onClick={addNewPost}>Создать пост</MyButton>
-            </form>
-            <PostList posts={posts1} title='Список постов № 1' />
-            {/* <PostList posts={posts2} title='Список постов № 2' /> */}
+            <PostForm create={createPost} />
+            <hr style={{ margin: "15px 15px" }} />
+            <div>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPost}
+                    defaultValue="Сортировка"
+                    options={[
+                        { value: 'title', name: 'По заголовку' },
+                        { value: 'body', name: 'По описанию' },
+                    ]}
+                />
+            </div>
+            {posts.length !== 0
+                ? <PostList remove={removePost} posts={posts} title='Список постов № 1' />
+                : <h2 style={{ textAlign: 'center', marginTop: '50px' }}>Постов нет! Увы(</h2>
+            }
         </div >
     );
 }
